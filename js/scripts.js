5aa9638db99662a0af226fa47e88101a755dcd87
Oscar Daniël Goossens <oscargoossens@users.noreply.github.com>
2021-09-17 17:04:52 +0200

    searchFilter working
    added some styling.
    Can't figure out what's wrong with the removing

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,19 @@
-baguetteBox.run('.gallery');
+baguetteBox.run('#gallery');
 
-let searchBar = document.getElementsByClassName('.search');
+let userInput = document.querySelector('.search');
 let a = document.querySelectorAll('a');
-
-
-let searchArray = Array.from(searchBar);
 let imageArray = Array.from(a);
-let userLower = searchArray[0].getAttribute("value").toLowerCase();
+
 
 const searchFilter = () => {
   for (i=0; i<imageArray.length; i++){
     let dataCaption = imageArray[i].getAttribute('data-caption').toLowerCase();
 
-      if (dataCaption.includes(userInput)) {
-        imageArray[i].style.display = 'block';
-      } else {
-        imageArray[i].style.display = 'none';
-      }
+
+    if (dataCaption.includes(userInput.value.toLowerCase())){
+      imageArray[i].style.display = 'block';
+    } else {
+      imageArray[i].style.display = 'none';
     }
-  };
+  }
+};
